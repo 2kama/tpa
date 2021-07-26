ec01a52a4601bc26e3a43b3409a724a42f726efc
Captain Developer <techbro104@gmail.com>
2021-07-26 23:59:21 +0100

    .

--- functions/cloud_functions/user.js
+++ functions/cloud_functions/user.js
@@ -38,7 +38,7 @@ exports.userCreated = functions.auth.user().onCreate(async (user) => {
         ROI : 0,
         assignedTrader : "",
         isApproved : false,
-        affiliate : "",
+        referralCode : "",
         affiliateCode : ""
     }
 
